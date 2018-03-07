78abdeb19c4b01b3ab392f9d816d767ef44dd0a8
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-03-07 17:25:26 +0000

    Add edit functionality to projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,12 +16,27 @@ class ProjectsController < ApplicationController
       render 'new'
     end
   end
-
-  def project_params
-    params.require(:project).permit(:name, :description)
-  end
   def show
     @project = Project.find(params[:id])
   end
 
+  def edit
+    @project = Project.find(params[:id])
+
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      flash[:success] = 'Project has been updated.'
+      redirect_to @project
+    else
+      flash[:danger] = 'Project has not been updated.'
+      render 'edit'
+    end
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
   root 'projects#index'
-  resources :projects, only: [:new, :create, :show]
+  resources :projects
 end
