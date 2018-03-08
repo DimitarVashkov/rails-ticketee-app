833c16233cd5034d2cc4782fffac7cd14040561a
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-03-08 15:46:19 +0000

    Add tickets display

---
+++ spec/features/viewing_tickets_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'User can view tickets' do
+  before do
+    sublime = create(:project, name: 'Sublime Text 3')
+    create(:ticket, project: sublime, name: 'Make it shiny!', description: 'Gradients!')
+
+    ie = create(:project, name: 'Internet Explorer')
+    create(:ticket, project: ie, name: 'Standards comp', description: 'Isnt a joke')
+
+    visit '/'
+  end
+
+  scenario 'for a given project' do
+    click_link 'Sublime Text 3'
+    expect(page).to have_content 'Make it shiny!'
+    expect(page).to_not have_content 'Standards comp'
+
+    click_link 'Make it shiny!'
+    within('#ticket h2') do
+      expect(page).to have_content 'Make it shiny!'
+    end
+    expect(page). to have_content 'Gradients!'
+  end
+end
