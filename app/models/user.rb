2619470765162bb851ddb95625fe20344cca235b
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-03-11 00:36:35 +0000

    Archived users cannot sign in to the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+
   def to_s
     "#{email} (#{admin ? 'Admin' : 'User'})"
   end
 
   def archive
     update(archived_at: Time.now)
+  end
+  def active_for_authentication?
+    super && archived_at.nil?
+  end
+  def inactive_message
+    archived_at.nil? ? super : :archived
   end
 end
