fc7ba27b0a9fb84a152fa9ef15b9a5ff7b94f288
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-03-11 01:25:42 +0000

    Handle authorization errors gracefully

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  private
+
+  def not_authorized
+    flash[:danger] = 'You are not allowed to do that.'
+    redirect_to root_url
+  end
 end
