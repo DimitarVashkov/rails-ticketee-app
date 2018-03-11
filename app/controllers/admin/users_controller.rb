4a848627e83fd811b61cf54d64a8cf43bbe8e895
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-03-11 00:20:21 +0000

    Admin can edit and update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,13 @@
 class Admin::UsersController < Admin::ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
   def index
     @users = User.order(:email)
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -16,7 +19,29 @@ class Admin::UsersController < Admin::ApplicationController
     end
   end
 
+  def show; end
+
+  def edit; end
+
+  def update
+    # passwords are stored as digests
+    # and we can't store a blank password
+    params[:user].delete(:password) if params[:user][:password].blank?
+
+    if @user.update(user_params)
+      flash[:success] = 'User has been updated.'
+      redirect_to admin_users_url
+    else
+      flash[:danger] = 'User has not been updated.'
+      render 'edit'
+    end
+  end
+
   def user_params
     params.require(:user).permit(:email, :password, :admin)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
