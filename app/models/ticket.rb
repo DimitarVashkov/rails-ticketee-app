3370336184f8569cb0aa13da5ba6319f7d29d228
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-03-13 14:49:02 +0000

    Tickets can have a state assigned when comments are created

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,5 +5,6 @@ class Ticket < ApplicationRecord
   validates :description, presence: true
   has_many :comments, dependent: :destroy
   has_many :attachments, dependent: :destroy
+  belongs_to :state
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
 end
