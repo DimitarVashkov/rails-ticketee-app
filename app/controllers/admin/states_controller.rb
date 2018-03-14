716ecfc67436ec2a3fa9eca1ded8908b4c840ac3
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-03-14 18:12:50 +0000

    Admins can now set a default state for tickets

--- app/controllers/admin/states_controller.rb
+++ app/controllers/admin/states_controller.rb
@@ -6,14 +6,21 @@ class Admin::StatesController < Admin::ApplicationController
     @state = State.new
   end
   def create
-    @state = State.new(state_params)
-    if @state.save
+  @state = State.new(state_params)
+  if @state.save
       flash[:notice] = 'State has been created.'
       redirect_to admin_states_path
     else
       flash.now[:alert] = 'State has not been created.'
       render 'new'
     end
+  end
+
+  def make_default
+    @state = State.find(params[:id])
+    @state.make_default!
+    flash[:notice] = "'#{@state.name}' is now the default state."
+    redirect_to admin_states_path
   end
 
   def state_params
